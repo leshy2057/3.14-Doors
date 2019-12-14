62208a0d9f88eccab434872b7710cfb04d35200e
XaXallle4ka <33667573+XaXallle4ka@users.noreply.github.com>
2019-12-14 13:55:20 +0300

    Начал делать анимацию(pyganim)

--- Classes/Settings.py
+++ Classes/Settings.py
@@ -1,5 +1,5 @@
 
-PLAYER_SIZE = (40, 50)
+PLAYER_SIZE = (32, 64)
 PLAYER_JUMP_FORCE = 7
 PLAYER_SPEED = 5
 
@@ -13,3 +13,14 @@ BLOCK_SIZE = (32, 32)
 WIN_WIDTH = 700  # Ширина создаваемого окна
 WIN_HEIGHT = 400  # Высота
 
+ANIMATION_DELAY = 0.1 # скорость смены кадров
+ANIMATION_RIGHT = [('run_r1.png'),
+            ('run_r2.png'),
+            ('run_r3.png')]
+ANIMATION_LEFT = [('run_l1.png'),
+            ('run_l2.png'),
+            ('run_l3.png')]
+ANIMATION_JUMP_LEFT = [('jump_l.png', 0.1)]
+ANIMATION_JUMP_RIGHT = [('jump_r.png', 0.1)]
+ANIMATION_JUMP = [('jump_r.png', 0.1)]
+ANIMATION_STAY = [('playerTest.png', 0.1)]
