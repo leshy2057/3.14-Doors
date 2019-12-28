19ab60034557d5dfe3cf06e4536b442e88ff5b86
leshy2057 <48924544+leshy2057@users.noreply.github.com>
2019-12-28 21:28:21 +0300

    0.1.0b3
    Начал вводить систему сохранений;

--- Classes/Levels.py
+++ Classes/Levels.py
@@ -44,7 +44,8 @@ class Menus:
             self.buttonMenu = Button(x=650, y=0, w=50, h=50, text="Menu", color=(230, 0, 0), onColor=(200, 0, 0), pressColor=(150, 0, 0), fontSize=25, func=self.ToMenu)
             self.levelButtons = [
                 Button(x=0, y=0, w=100, h=100, text="1", color=(230, 230, 230), onColor=(200, 200, 200), pressColor=(150, 150, 150), func=lambda: self.ToGame("level_1")),
-                Button(x=100, y=0, w=100, h=100, text="2", color=(230, 230, 230), onColor=(200, 200, 200), pressColor=(150, 150, 150), func=lambda: self.ToGame("level_2"))
+                Button(x=100, y=0, w=100, h=100, text="2", color=(230, 230, 230), onColor=(200, 200, 200), pressColor=(150, 150, 150), func=lambda: self.ToGame("level_2")),
+                Button(x=200, y=0, w=100, h=100, text="3", color=(230, 230, 230), onColor=(200, 200, 200), pressColor=(150, 150, 150), func=lambda: self.ToGame("level_3"))
             ]
 
         def ToGame(self, level_name):
@@ -95,8 +96,12 @@ class Menus:
             time.sleep(PAUSE_TO_LOAD)
 
         def GenerateLevel(self):
-            with open("Levels\\levels.json", "r") as levels:
-                self.level = json.load(levels)[self.level_name]
+            try:
+                with open("Levels\\levels.json", "r") as levels:
+                    self.level = json.load(levels)[self.level_name]
+            except:
+                raise Exception("Level not found in json!")
+
             
             x, y, step = 0, 0, 32
             level_for = ""
@@ -137,91 +142,3 @@ class Menus:
             self.buttonMenu.Update(self.surface)
     
             windows.blit(self.surface, (0, 0))
-
-                        
-
-
-
-
-"""
-import requests, threading, time, json, random, pygame, sys
-from Classes.Player import Player
-from Classes.Block import Block
-from Classes.Camera import *
-from Classes.Settings import *
-
-
-map = []
-player = Player()
-
-
-pygame.init()
-clock = pygame.time.Clock()
-windows = pygame.display.set_mode((700, 400))
-pygame.display.set_caption("Test")
-
-surface = pygame.Surface((WIN_WIDTH, WIN_HEIGHT))
-
-blocks = pygame.sprite.Group()
-wallList = pygame.sprite.Group()
-platforms = []
-
-level = [
-       "-                           -|",
-       "-                           -|",
-       "-                           -|",
-       "-                           -|",
-       "-                           -|",
-       "-                       -----|",
-       "-                   --  -----|",
-       "---------        --     -----|",
-       "-           ---         -----|",
-       "-                       -----|",
-]
-
-x, y, step = 0, 0, 32
-level_for = ""
-for i in level: level_for += i
-for symbol in level_for:
-    if (symbol == "-"):
-        block = Block(x, y)
-        blocks.add_internal(block)
-        wallList.add_internal(block)
-        x += step
-    elif (symbol == "|"):
-        x = 0
-        y += step
-    else:
-        x += step
-blocks.add_internal(player)
-player.walls = wallList
-
-
-total_level_width = len(level[0]) * BLOCK_SIZE[0]  # Высчитываем фактическую ширину уровня
-total_level_height = len(level) * BLOCK_SIZE[1]  # высоту
-camera = Camera(camera_configure, total_level_width, total_level_height)
-
-
-pygame.key.set_repeat(1, 10)
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            runGame = False
-            sys.exit()
-
-    player.Move(pygame.key.get_pressed())
-    player.update()
-    surface.fill((255, 255, 255))
-
-    camera.update(player)
-
-    for i in blocks:
-        surface.blit(i.image, camera.apply(i))
-    
-    windows.blit(surface, (0, 0))
-    pygame.display.flip()
-    clock.tick(30)
-
-
-pygame.quit()
-"""
